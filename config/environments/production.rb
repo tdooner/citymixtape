2c281f8ce9e0c1353b42a22ceb7363566269e6b6
Tom Dooner <tom.dooner@brigade.com>
2016-02-01 22:26:18 -0800

    Use rails_12factor gem instead of logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
-  config.logger = Logger.new(STDOUT)
+  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store

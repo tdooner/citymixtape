1f34defe316217cd1e61253bfa38b0e207dc48de
Tom Dooner <tom.dooner@brigade.com>
2016-02-01 22:46:25 -0800

    Paginate MetroAreaSearchResult

--- app/models/metro_area_search_result.rb
+++ app/models/metro_area_search_result.rb
@@ -2,9 +2,20 @@ class MetroAreaSearchResult < ApplicationRecord
   self.primary_key = :metro_area_id
 
   def self.search(metro_area_id)
-    # TODO: Handle more than 50 events
-    res = find_by(metro_area_id: metro_area_id) ||
-      create(metro_area_id: metro_area_id, results: SONGKICK.metro_areas_events(metro_area_id).results.to_json)
+    unless res = find_by(metro_area_id: metro_area_id)
+      page = 1
+      more_results = true
+      results = []
+
+      while more_results
+        api_response = SONGKICK.metro_areas_events(metro_area_id, page: page)
+        results += api_response.results
+        page += 1
+        more_results = results.length < api_response.total_entries && api_response.results.any?
+      end
+
+      res = create(metro_area_id: metro_area_id, results: results.to_json)
+    end
 
     # TODO: filter out "status":"cancelled"
     JSON.parse(res.results)

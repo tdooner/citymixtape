0228dd3de47b00601f31b85b5e699efc45059cc0
Tom Dooner <tom.dooner@brigade.com>
2016-02-03 22:35:32 -0800

    Fix sidekiq initialization logic
    This allows producing into the queue despite not having the consumer run
    in your thread.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 require 'sidekiq/cli'
 
-if ENV['REDIS_URL'] && ENV['SIDEKIQ_CONCURRENCY']
+if ENV['REDIS_URL'] && Rails.env.production?
   Sidekiq.configure_server do |config|
     config.redis = { url: ENV['REDIS_URL'] }
   end
@@ -9,11 +9,13 @@ if ENV['REDIS_URL'] && ENV['SIDEKIQ_CONCURRENCY']
     config.redis = { url: ENV['REDIS_URL'] }
   end
 
-  Thread.abort_on_exception = true
-  Thread.new do
-    sidekiq = Sidekiq::CLI.instance
-    sidekiq.parse(['-c', ENV['SIDEKIQ_CONCURRENCY']])
-    sidekiq.run
+  if concurrency = ENV['SIDEKIQ_CONCURRENCY']
+    Thread.abort_on_exception = true
+    Thread.new do
+      sidekiq = Sidekiq::CLI.instance
+      sidekiq.parse(['-c', concurrency])
+      sidekiq.run
+    end
   end
 
   Rails.application.config.active_job.queue_adapter = :sidekiq

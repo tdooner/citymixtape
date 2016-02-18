9802243b87d20c60aa1e2fe354bbe8122a5dc64c
Tom Dooner <tom.dooner@brigade.com>
2016-02-17 22:50:49 -0800

    Change commandline_options to Array

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Mytownplaylist
   class Application < Rails::Application
-    config.browserify_rails.commandline_options = '-t reactify'
+    config.browserify_rails.commandline_options = ['-t reactify']
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
